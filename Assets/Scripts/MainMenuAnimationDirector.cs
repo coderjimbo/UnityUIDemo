7285b694156b8d1504bc42d0c5df6497b8ad2956
James Keith <keithj@mail.usf.edu>
2018-08-25 00:43:11 -0400

    Initial Commit!

---
+++ Assets/Scripts/MainMenuAnimationDirector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuAnimationDirector : MonoBehaviour {
+    public Text contentSectionTitle;
+    public GameObject contentSectionContent;
+
+    public GameObject singleplayerContentPrefab;
+    public string singleplayerContentTitle;
+
+    public GameObject settingsContentPrefab;
+    public string settingsContentTitle;
+
+    private Animator mainMenuAnimator;
+
+    // Use this for initialization
+    void Start () {
+        mainMenuAnimator = gameObject.GetComponent<Animator>();
+	}
+
+    private void SwitchToContent(GameObject prefab, string contentTitle)
+    {
+        contentSectionTitle.text = contentTitle;
+        GameObject.Instantiate(prefab, contentSectionContent.transform, false);
+    }
+	
+	public void HideMainMenu()
+    {
+        mainMenuAnimator.SetTrigger("Hide");
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuAnimator.SetTrigger("Show");
+    }
+
+    public void ShowSingleplayer()
+    {
+        HideMainMenu();
+        mainMenuAnimator.SetTrigger("Singleplayer");
+    }
+
+    public void ShowSettings()
+    {
+        HideMainMenu();
+        mainMenuAnimator.SetTrigger("Settings");
+    }
+
+    public void SwitchToSingleplayer()
+    {
+        SwitchToContent(singleplayerContentPrefab, singleplayerContentTitle);
+        ShowSingleplayer();
+    }
+
+    public void SwitchToSettings()
+    {
+        SwitchToContent(settingsContentPrefab, settingsContentTitle);
+        ShowSettings();
+    }
+}
---
+++ Assets/Scripts/Scrolling Background/ScrollUITexture.cs
@@ -0,0 +1,34 @@
+﻿/******************************************************************************************************/
+/* ScrollUITexture.cs																				  */
+/* Purpose:																							  */
+/*																									  */
+/******************************************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollUITexture : MonoBehaviour {
+    public float scrollSpeed = 0.5f;
+    public float scaleFactor = 100;
+
+    private RawImage imageToMove;
+    private float scrollTime = 0.0f;
+
+    void Start()
+    {
+        imageToMove = gameObject.GetComponent<RawImage>();
+    }
+
+    void Update()
+    {
+        scrollTime = Time.time * scrollSpeed;
+        imageToMove.uvRect = new Rect(
+            scrollTime,
+            scrollTime, 
+            imageToMove.rectTransform.rect.width / scaleFactor,
+            imageToMove.rectTransform.rect.height / scaleFactor
+        );
+    }
+}
